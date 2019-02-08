1fbc1401cc03d83c3b62486d077c2e8dee8c781f
Nicole Erler <n.erler@erasmusmc.nl>
2019-02-08 21:23:08 +0100

    update

--- CopyFiles.R
+++ CopyFiles.R
@@ -1,18 +1,35 @@
 # Practical: Incomplete Data ---------------------------------------------------
-file.copy('materials/Practicals/IncompleteData/Practical_IncompleteData.html',
-          'static/practical/IncompleteData', overwrite = TRUE)
+if (!dir.exists("EP16website/static/practical/IncompleteData"))
+  dir.create("EP16website/static/practical/IncompleteData")
 
-webshot::webshot('materials/Practicals/IncompleteData/Practical_IncompleteData.html',
-                 'static/practical/IncompleteData/image.png',
+file.copy('Practicals/IncompleteData/Practical_IncompleteData.html',
+          'EP16website/static/practical/IncompleteData', overwrite = TRUE)
+
+webshot::webshot('Practicals/IncompleteData/Practical_IncompleteData.html',
+                 'EP16website/static/practical/IncompleteData/image.png',
                  vwidth = 800, vheight = 450,
                  cliprect = 'viewport', zoom = 2)
 
 # Practical: MI with mice ------------------------------------------------------
-file.copy('materials/Practicals/MImice/Practical_MImice.html',
-          'static/practical/MImice', overwrite = TRUE)
+if (!dir.exists("EP16website/static/practical/MImice"))
+  dir.create("EP16website/static/practical/MImice")
 
-webshot::webshot('materials/Practicals/MImice/Practical_MImice.html',
-                 'static/practical/MImice/image.png',
+file.copy('Practicals/MImice/Practical_MImice.html',
+          'EP16website/static/practical/MImice', overwrite = TRUE)
+
+webshot::webshot('Practicals/MImice/Practical_MImice.html',
+                 'EP16website/static/practical/MImice/image.png',
                  vwidth = 800, vheight = 450,
                  cliprect = 'viewport', zoom = 2)
 
+
+
+# Slides -----------------------------------------------------------------------
+if (!dir.exists("EP16website/static/slide/lecture"))
+  dir.create("EP16website/static/slide/lecture")
+
+file.copy('Slides/MICourse_Slides.pdf',
+          'EP16website/static/slide/lecture', overwrite = TRUE)
+
+webshot::webshot('Slides/MICourse_Slides.pdf',
+                 'EP16website/static/slide/lecture/image.png')
